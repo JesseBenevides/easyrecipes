7efaf2d79ffd77bf7eef3cbaf6ea1a3f186fc559
Jesse Benevides <jessebenevides@gmail.com>
2021-12-10 11:49:17 -0300

    Implementa requisições às APIs para buscar as receitas por categoria

--- src/services/drinkAPI.js
+++ src/services/drinkAPI.js
@@ -1,5 +1,6 @@
 const ENDPOINT_DRINKS_BY_NAME = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-const ENDPOINT_DRINKS_CATEGORIES = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+const ENDPOINT_LIST_CATEGORIES = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+const ENDPOINT_DRINKS_BY_CATEGORY = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
 
 export async function fetchDrinkByName(drinkName) {
   const ENDPOINT = `${ENDPOINT_DRINKS_BY_NAME}${drinkName}`;
@@ -10,7 +11,14 @@ export async function fetchDrinkByName(drinkName) {
 }
 
 export async function fetchDrinksCategories() {
-  const response = await fetch(ENDPOINT_DRINKS_CATEGORIES);
+  const response = await fetch(ENDPOINT_LIST_CATEGORIES);
+  const { drinks } = await response.json();
+  return drinks;
+}
+
+export async function fetchDrinksByCategories(categoryName) {
+  const ENDPOINT = `${ENDPOINT_DRINKS_BY_CATEGORY}${categoryName}`;
+  const response = await fetch(ENDPOINT);
   const { drinks } = await response.json();
   return drinks;
 }
--- src/services/mealAPI.js
+++ src/services/mealAPI.js
@@ -1,5 +1,6 @@
 const ENDPOINT_MEAL_BY_NAME = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-const ENDPOINT_MEALS_CATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+const ENDPOINT_LIST_CATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+const ENDPOINT_MEALS_BY_CATEGORY = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
 
 export async function fetchMealByName(mealName) {
   const ENDPOINT = `${ENDPOINT_MEAL_BY_NAME}${mealName}`;
@@ -10,7 +11,14 @@ export async function fetchMealByName(mealName) {
 }
 
 export async function fetchMealsCategories() {
-  const response = await fetch(ENDPOINT_MEALS_CATEGORIES);
+  const response = await fetch(ENDPOINT_LIST_CATEGORIES);
+  const { meals } = await response.json();
+  return meals;
+}
+
+export async function fetchMealsByCategories(categoryName) {
+  const ENDPOINT = `${ENDPOINT_MEALS_BY_CATEGORY}${categoryName}`;
+  const response = await fetch(ENDPOINT);
   const { meals } = await response.json();
   return meals;
 }

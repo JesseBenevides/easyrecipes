7297475cfb4b0b58005967df45bd8804d33d909e
Jesse Benevides <jessebenevides@gmail.com>
2021-12-10 10:20:06 -0300

    Adiciona requisições às categorias nas APIs

--- src/services/drinkAPI.js
+++ src/services/drinkAPI.js
@@ -1,9 +1,16 @@
 const ENDPOINT_DRINKS_BY_NAME = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const ENDPOINT_DRINKS_CATEGORIES = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 
-export default async function fetchDrinkByName(drinkName) {
+export async function fetchDrinkByName(drinkName) {
   const ENDPOINT = `${ENDPOINT_DRINKS_BY_NAME}${drinkName}`;
 
   const response = await fetch(ENDPOINT);
+  const { drinks } = await response.json();
+  return drinks;
+}
+
+export async function fetchDrinksCategories() {
+  const response = await fetch(ENDPOINT_DRINKS_CATEGORIES);
   const { drinks } = await response.json();
   return drinks;
 }
--- src/services/mealAPI.js
+++ src/services/mealAPI.js
@@ -1,9 +1,16 @@
 const ENDPOINT_MEAL_BY_NAME = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const ENDPOINT_MEALS_CATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
-export default async function fetchMealByName(mealName) {
+export async function fetchMealByName(mealName) {
   const ENDPOINT = `${ENDPOINT_MEAL_BY_NAME}${mealName}`;
 
   const response = await fetch(ENDPOINT);
+  const { meals } = await response.json();
+  return meals;
+}
+
+export async function fetchMealsCategories() {
+  const response = await fetch(ENDPOINT_MEALS_CATEGORIES);
   const { meals } = await response.json();
   return meals;
 }

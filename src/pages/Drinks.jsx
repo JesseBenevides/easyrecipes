a8cf4a68b7ee72bcadbe801b24e39a66ee448c7b
Jesse Benevides <jessebenevides@gmail.com>
2021-12-10 11:33:50 -0300

    Implementa a rendereização dos FilterButtons nas páginas de Comidas e Bebidas

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,15 +1,24 @@
 import React, { useContext } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import CategoriesFilterButtons from '../components/CategoriesFilterButtons';
 import RecipeCard from '../components/RecipeCard';
 import RecipesContext from '../context/RecipesContext';
 import useAPI from '../hooks/useAPI';
-import { fetchDrinkByName } from '../services/drinkAPI';
+import { fetchDrinkByName, fetchDrinksCategories } from '../services/drinkAPI';
 
 function Drinks() {
-  const { recipes: { drinkList, setDrinkList } } = useContext(RecipesContext);
+  const { recipes: {
+    drinkList,
+    drinkCategories,
+    setDrinkList,
+    setDrinkCategories,
+  } } = useContext(RecipesContext);
+
   const INICIAL_DRINKLIST_LENGTH = 12;
+  const INICIAL_DRINKCATEGORIES_LENGTH = 5;
 
   useAPI(fetchDrinkByName, setDrinkList, '', INICIAL_DRINKLIST_LENGTH);
+  useAPI(fetchDrinksCategories, setDrinkCategories, '', INICIAL_DRINKCATEGORIES_LENGTH);
 
   const renderDrinkCard = () => (
     drinkList.map((recipe, index) => (
@@ -22,9 +31,18 @@ function Drinks() {
     ))
   );
 
+  const renderDrinkCategories = () => (
+    <CategoriesFilterButtons categoryList={ drinkCategories } />
+  );
+
   return (
     <section>
       <h2>Lista de Coquetéis</h2>
+      <Container>
+        <Row>
+          {renderDrinkCategories()}
+        </Row>
+      </Container>
       <Container>
         <Row>
           { renderDrinkCard() }
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import CategoriesFilterButtons from '../components/CategoriesFilterButtons';
 import RecipeCard from '../components/RecipeCard';
 import RecipesContext from '../context/RecipesContext';
 import useAPI from '../hooks/useAPI';
-import { fetchMealByName } from '../services/mealAPI';
+import { fetchMealByName, fetchMealsCategories } from '../services/mealAPI';
 
 function Foods() {
   const { recipes: {
@@ -14,8 +15,11 @@ function Foods() {
   } } = useContext(RecipesContext);
 
   const INICIAL_FOODLIST_LENGTH = 12;
+  const INICIAL_FOODCATEGORIES_LENGTH = 5;
 
   useAPI(fetchMealByName, setFoodList, '', INICIAL_FOODLIST_LENGTH);
+  useAPI(fetchMealsCategories, setFoodCategories, '', INICIAL_FOODCATEGORIES_LENGTH);
+
   const renderFoodCard = () => (
     foodList.map((recipe, index) => (
       <RecipeCard
@@ -27,9 +31,18 @@ function Foods() {
     ))
   );
 
+  const renderFoodCategories = () => (
+    <CategoriesFilterButtons categoryList={ foodCategories } />
+  );
+
   return (
     <section>
       <h2>Lista de Comidas</h2>
+      <Container>
+        <Row>
+          {renderFoodCategories()}
+        </Row>
+      </Container>
       <Container>
         <Row>
           { renderFoodCard() }

354ac002083d70e0e4182bf2251cdcc06cace65d
Jesse Benevides <jessebenevides@gmail.com>
2021-12-10 09:58:13 -0300

    Implementa a página para a rota '/bebidas'

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipeCard from '../components/RecipeCard';
+import RecipesContext from '../context/RecipesContext';
+import useAPI from '../hooks/useAPI';
+import fetchDrinkByName from '../services/drinkAPI';
 
 function Drinks() {
-  return <div />;
+  const { recipes: { drinkList, setDrinkList } } = useContext(RecipesContext);
+
+  useAPI(fetchDrinkByName, setDrinkList, '');
+
+  const renderDrinkCard = () => (
+    drinkList.map((recipe, index) => (
+      <RecipeCard
+        key={ `${index}` }
+        name={ recipe.strDrink }
+        image={ recipe.strDrinkThumb }
+        index={ index }
+      />
+    ))
+  );
+
+  return (
+    <section>
+      <h2>Lista de Coquetéis</h2>
+      { renderDrinkCard() }
+    </section>
+  );
 }
 
 export default Drinks;

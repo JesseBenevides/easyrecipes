8706f54481660bf1d8cb69a006f1a37ef8e89f4c
Jesse Benevides <jessebenevides@gmail.com>
2021-12-10 09:57:55 -0300

    Implementa a página para a rota '/comidas'

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipeCard from '../components/RecipeCard';
+import RecipesContext from '../context/RecipesContext';
+import useAPI from '../hooks/useAPI';
+import fetchMealByName from '../services/mealAPI';
 
 function Foods() {
+  const { recipes: { foodList, setFoodList } } = useContext(RecipesContext);
+
+  useAPI(fetchMealByName, setFoodList, '');
+  const renderFoodCard = () => (
+    foodList.map((recipe, index) => (
+      <RecipeCard
+        key={ `${index}` }
+        name={ recipe.strMeal }
+        image={ recipe.strMealThumb }
+        index={ index }
+      />
+    ))
+  );
+
   return (
-    <div />
+    <section>
+      <h2>Lista de Comidas</h2>
+      { renderFoodCard() }
+    </section>
   );
 }
 

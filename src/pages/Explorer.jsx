e8fa21a3d76d3e33b613e5494c2ad9768c9e6e76
PedroAugmed <pedroaugmed7@gmail.com>
2021-12-10 16:24:11 -0300

    Comite para habilitar git pull

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -4,6 +4,13 @@ import Footer from '../components/Footer';
 function Explorer() {
   return (
     <div>
+       <button 
+          data-testid="explore-food"
+          // disabled={ disable }
+          onClick={ this.inputSubmite }
+        >
+          Explorar Comidas
+        </button>
       <Footer />
     </div>
   );

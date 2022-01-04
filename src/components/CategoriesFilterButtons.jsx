fbec32a75b9bb00ff7838d6895458a9d1f3f8d1d
Jesse Benevides <jessebenevides@gmail.com>
2022-01-04 19:12:23 -0300

    Lista as receitas por categoria ao clicar em algum botão de filtro

--- src/components/CategoriesFilterButtons.jsx
+++ src/components/CategoriesFilterButtons.jsx
@@ -2,8 +2,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, ButtonGroup, Container, Row } from 'react-bootstrap';
+import { useContext } from 'react';
+import RecipesContext from '../context/RecipesContext';
+import { fetchMealsByCategories } from '../services/mealAPI';
+import { fetchDrinksByCategories } from '../services/cocktailAPI';
 
-function CategoriesFilterButtons({ categoryList }) {
+function CategoriesFilterButtons({ categoryList, recipeType }) {
+  const { recipes } = useContext(RecipesContext);
+
+  const {
+    setFoodList,
+    setDrinkList,
+  } = recipes;
+  async function getRecipesByCategory(categoryName, recipeType ) {
+    let recipeList;
+    switch(recipeType) {
+      case 'food':
+         recipeList = await fetchMealsByCategories(categoryName);
+         setFoodList(recipeList);
+        break;
+      
+      case 'drink':
+        recipeList = await fetchDrinksByCategories(categoryName);
+        setDrinkList(recipeList)
+        break;
+      
+      default:
+        break;
+    }
+  }
+
+  function handleClick({ target: { name } }) {
+    getRecipesByCategory(name, recipeType)
+  }
   return (
     <Container>
       <Row>
@@ -20,6 +51,8 @@ function CategoriesFilterButtons({ categoryList }) {
             <Button
               variant="secondary"
               data-testid={ `${category.strCategory}-category-filter` }
+              name={ category.strCategory }
+              onClick={ handleClick }
             >
               {category.strCategory}
             </Button>
@@ -32,6 +65,7 @@ function CategoriesFilterButtons({ categoryList }) {
 
 CategoriesFilterButtons.propTypes = {
   categoryList: PropTypes.arrayOf(PropTypes.any).isRequired,
+  recipeType: PropTypes.string.isRequired,
 };
 
 export default CategoriesFilterButtons;

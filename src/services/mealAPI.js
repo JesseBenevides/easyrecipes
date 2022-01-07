8d7bfa8401985160d6f4a610f95737f33d226d66
Jesse Benevides <jessebenevides@gmail.com>
2022-01-07 11:52:30 -0300

    Adiciona services fechtMealsIngredients e fetchDrinksIngredients

--- src/services/mealAPI.js
+++ src/services/mealAPI.js
@@ -4,6 +4,7 @@ const ENDPOINT_MEALS_BY_CATEGORY = 'https://www.themealdb.com/api/json/v1/1/filt
 const ENDPOINT_MEALS_BY_FIRST_LETTER = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
 const ENDPOINT_MEAL_RANDOM = 'https://www.themealdb.com/api/json/v1/1/random.php';
 const ENDPOINT_MEALS_BY_INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+const ENDPOINT_LIST_MEALS_INGREDIENTS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
 
 export async function fetchMealByName(mealName) {
   const ENDPOINT = `${ENDPOINT_MEAL_BY_NAME}${mealName}`;
@@ -41,6 +42,12 @@ export async function fetchMealsByFirstLetter(firstLetter) {
 
 export async function fetchMealRandom() {
   const response = await fetch(ENDPOINT_MEAL_RANDOM);
+  const { meals } = await response.json();
+  return meals;
+}
+
+export async function fetchMealsIngredients() {
+  const response = await fetch(ENDPOINT_LIST_MEALS_INGREDIENTS);
   const { meals } = await response.json();
   return meals;
 }

cc14463566aa336a7432d9a4d852d86a17cbf11d
Jesse Benevides <jessebenevides@gmail.com>
2022-01-11 18:46:00 -0300

    Estiliza buttons das páginas de explorar

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,39 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { fetchDrinkRandom } from '../services/cocktailAPI';
 
-function ExploreDrinks(props) {
-  function redirectTo(path) {
-    const { history } = props;
-    history.push(path);
-  }
+function ExploreDrinks() {
+  const history = useHistory();
+
   async function getDrinkRandom() {
     const drinkArray = await fetchDrinkRandom();
     const { idDrink } = drinkArray[0];
-    redirectTo(`/bebidas/${idDrink}`);
+    const path = `/bebidas/${idDrink}`;
+    history.push(path);
   }
   return (
-    <div>
+    <>
       <Header pageTitle="Explorar Bebidas" />
-      <button
-        type="button"
-        data-testid="explore-by-ingredient"
-        onClick={ () => redirectTo('/explorar/bebidas/ingredientes') }
-      >
-        Por Ingredientes
-      </button>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <Button
+          className="w-75 mb-2 mt-4"
+          size="lg"
+          variant="secondary"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+        >
+          Por Ingredientes
+        </Button>
 
-      <button
-        type="button"
-        data-testid="explore-surprise"
-        onClick={ () => getDrinkRandom() }
-      >
-        Me Surpreenda!
-      </button>
+        <Button
+          className="w-75 mb-2"
+          size="lg"
+          variant="secondary"
+          data-testid="explore-surprise"
+          onClick={ () => getDrinkRandom() }
+        >
+          Me Surpreenda!
+        </Button>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
